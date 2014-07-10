6ebf2a60d77f4570bc5e9c1a45165b6a1f40c3de
bradleypj@icloud.com <bradleypj@icloud>
2014-07-10 10:07:56 -0700

    changed network controller's method order

---
+++ Swift-GithubToGo/RepoSearchTVCell.swift
@@ -0,0 +1,29 @@
+//
+//  RepoSearchTVCell.swift
+//  Swift-GithubToGo
+//
+//  Created by Bradley Johnson on 6/8/14.
+//  Copyright (c) 2014 Bradley Johnson. All rights reserved.
+//
+
+import UIKit
+
+class RepoSearchTVCell: UITableViewCell {
+
+    init(style: UITableViewCellStyle, reuseIdentifier: String) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // Initialization code
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
